f4bf0df2c55d820ff454aa63a8b48185f63653ba
986114497@qq.com <986114497@qq.com>
2018-12-22 17:06:31 +0800

    修改

--- app/src/main/java/com/example/a98611/test/Http.java
+++ app/src/main/java/com/example/a98611/test/Http.java
@@ -2,13 +2,25 @@ package com.example.a98611.test;
 
 import android.content.Context;
 /**
- *这是测试类
- *测试冲突代码
- *解决
- *作者
- *时间
- *邮箱
- *名称
+  * private String id;
+
+  * private String senderName;//发送人姓名
+
+  * private String title;//不能超过120个中文字符
+
+  * private String content;//邮件正文
+
+  * private String attach;//附件，如果有的话
+
+  * private String totalCount;//总发送人数
+
+  * private String successCount;//成功发送的人数
+
+  * private Integer isDelete;//0不删除 1删除
+
+  * private Date createTime;//目前不支持定时 所以创建后即刻发送
+
+  * privateSet<EmailList> EmailList;
  */
 public class Http {
     TransactionCallback successCallback;
